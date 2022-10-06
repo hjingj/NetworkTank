0b06c1cc923a4006814bf99de03ab1ac31384bc8
hjingj <hjingj@gmail.com>
2022-10-06 16:16:42 +0800

    Tank10 feat: Main Menu1-Join Button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 namespace Span
 {
     public class MainMenu : MonoBehaviourPunCallbacks
     {
         static MainMenu instance;
+        private GameObject m_ui;
+        private Button m_joinGameButton;
+
         void Awake()
         {
             if (instance != null)
@@ -17,6 +21,12 @@ namespace Span
             }
             instance = this;
             DontDestroyOnLoad(gameObject);
+
+            m_ui = transform.FindAnyChild<Transform>("UI").gameObject;
+            m_joinGameButton = transform.FindAnyChild<Button>("JoinGameButton");
+
+            m_ui.SetActive(true);
+            m_joinGameButton.interactable = false;
         }
     }
 }
